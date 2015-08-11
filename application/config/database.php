6a522b6c3058b5b47583212427b801767295a308
Laurensius D.S <burglar.tea@gmail.com>
2015-08-11 07:58:49 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'genitdev_asma123';
-$db['default']['password'] = '-8536]1wSP';
-$db['default']['database'] = 'genitdev_asma123';
+$db['default']['hostname'] = '*********';
+$db['default']['username'] = '*********';
+$db['default']['password'] = '*********';
+$db['default']['database'] = '*********';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
